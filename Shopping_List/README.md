f277acb3ed4c64826bf42e92d0267382d91c6d4e
bashhack <info@marclaughton.com>
2016-01-17 16:06:52 -0600

    refactoring shopping list app

--- Shopping_List/README.md
+++ Shopping_List/README.md
@@ -5,3 +5,6 @@
 * Enter the word DONE - in all caps - to quit the program
 * And, once I quit, I want the app to show me everything that's on my list
 * Allow user to type 'SHOW' and 'HELP' for extra functionality
+
+## Extras
+* I wanted to allow users to remove an item
--- Shopping_List/app.py
+++ Shopping_List/app.py
@@ -9,40 +9,53 @@ shopping_list = []
 
 
 # Functions
-def show():
-    print('Your list right now:')
+def clean_list():
     for item in shopping_list:
-
         if (item == 'SHOW' or item == 'DONE' or
-                item == 'HELP' or item == 'REMOVE'):
-            continue
+                item == 'HELP' or item == 'REMOVE' or item == 'COUNT'):
+            shopping_list.remove(item)
+
+
+def show_list():
+    clean_list()
+    for item in shopping_list:
         print(item)
 
 
-def help():
+def get_help():
     print('''
 At the prompt ('>'), enter your item to add.
 To show your list, type 'SHOW', 'REMOVE' to remove an item,
-and to view this help message at any time, type 'HELP\'
+'COUNT' to show the number of items in your list, and to view
+this help message at any time, type 'HELP\'
 ''')
 
 
 def remove_item():
-    show()
+    show_list()
     item_to_remove = input('Which item would you like to remove? ')
-    shopping_list.remove(item_to_remove)
-    print(item_to_remove, 'removed from your list.')
+    try:
+        shopping_list.remove(item_to_remove)
+        print(item_to_remove, 'removed from your list.')
+    except ValueError:
+        print('Item not found.')
+
+
+def get_item_count():
+    clean_list()
+    print('Your list has {} items'.format(len(shopping_list)))
 
 # Let user know how to use the script
 print('To begin, simply type the name of the item you\'d like to add.')
 print('Type \'HELP\' for information about this app, and type \'SHOW\'' +
-      'to see the current state of your list. Enjoy!')
+      ' to see the current state of your list. Enjoy!')
 print('To quit, type \'DONE\'')
 
 while True:
     # Get item
     if len(shopping_list) == 0:
-        item_to_add = input('What would you like to add? ')
+        print('What would you like to add?')
+        item_to_add = input('> ')
     else:
         item_to_add = input('> ')
 
@@ -50,32 +63,24 @@ while True:
     shopping_list.append(item_to_add)
 
     # Show help message to user
-    if 'HELP' in shopping_list:
-        help()
-        shopping_list.remove('HELP')
-
+    if item_to_add == 'HELP':
+        get_help()
+        shopping_list.remove(item_to_add)
+        continue
     # Show current state of list
-    if 'SHOW' in shopping_list:
-        show()
-        shopping_list.remove('SHOW')
-
+    elif item_to_add == 'SHOW':
+        print('Your current list:')
+        show_list()
+        continue
     # Remove item(s)
-    if 'REMOVE' in shopping_list:
+    elif item_to_add == 'REMOVE':
         remove_item()
-        shopping_list.remove('REMOVE')
-
+        continue
+    elif item_to_add == 'COUNT':
+        get_item_count()
+        continue
     # Quit based on user input
-    if 'DONE' in shopping_list:
-
+    elif item_to_add == 'DONE':
         print('Your completed list:')
-
-        for item in shopping_list:
-
-            # Prevent DONE from printing
-            if item == 'DONE':
-                continue
-
-            # Print itemized list
-            print(item)
-
+        show_list()
         break
